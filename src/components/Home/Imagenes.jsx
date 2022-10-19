c889521a0fc9f70ee0cbcb6eb38472286056f94b
Edison Parra <edisonparraortiz8@gmail.com>
2022-10-19 04:32:24 -0500

    add

--- src/components/Home/Imagenes.jsx
+++ src/components/Home/Imagenes.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import './styles/imagenes.css'
 import imagenfresa from'..//../../public/graphics/Imagen-producto fresa.png'
+import logoAzul from'..//../../public/graphics/logo-azul.png'
+import logoCafe from '..//../../public/graphics/logo-cafe.png'
+import logoScroll from '..//../../public/graphics/logo-scroll.png'
 
 const Imagenes = () => {
   return (
     <div className='contenedor-imagenes'>
-        <img src={imagenfresa} alt="imagen_fresa" className="imagenfresa" />
+       
+        <div className='información-imagenes'>
+          <img className='imagen_fresa' src={imagenfresa} alt="imagen_fresa" />
+          <div className='contenedor_sabores'>
+           <p className='p-sabor'>Sabor a:</p>
+           <a className='boton__fresa'href="#">Fresa</a>
+           <a className='boton__guayaba'href="#">Guayaba</a>
+           <a className='boton__toronja'href="#">Toronja</a>
+          </div>
+          <div className="descripcion">
+            <p className="titulo-vitalinea">Vitalinea® Bebible Fresa 217 gr.</p>
+            <p className="parrafo-descripcion">
+            iLa presentacién ideal para llevar contigo! Vitalinea ebible es la opción si eres de las personas que siempre estn activas y quieren probar algo sano, rico y práctico.
+            </p>
+            <div className="botones-logo">
+              <a className='boton-walmart' href="#">Comprar en walmart</a>
+              <img className='logo-azul' src={logoAzul} alt="logo azul" />
+              <img className='logo-cafe' src={logoCafe} alt="logo cafe" />
+              <img className='logo-scroll' src={logoScroll} alt="logo scroll" />
+
+            </div>
+          </div>
+
+        </div>
+
     </div>
   )
 }
